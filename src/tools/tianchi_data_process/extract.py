dd82d21bbd7d582215e35cc76843f8c4466b8394
luanshiyinyang <1695735420@qq.com>
2020-06-25 10:45:58 +0800

    new feature scale method

--- src/tools/tianchi_data_process/extract.py
+++ src/tools/tianchi_data_process/extract.py
@@ -9,10 +9,26 @@ Tianchi 的npy数据：T:N，D:1024 - > 100,1024
 import numpy as np
 import os
 from tqdm import tqdm
+import scipy
+import scipy.interpolate
 
 
 def load_npy(npy_file):
     return np.load(npy_file)
+
+
+def resize_feature(inputData, newSize=100):
+    # refer to DBG code
+    # inputX: (temporal_length,feature_dimension) #
+    originalSize = len(inputData)
+    if originalSize == 1:
+        inputData = np.reshape(inputData, [-1])
+        return np.stack([inputData] * newSize)
+    x = np.array(range(originalSize))
+    f = scipy.interpolate.interp1d(x, inputData, axis=0)
+    x_new = [i * float(originalSize - 1) / (newSize - 1) for i in range(newSize)]
+    y_new = f(x_new)
+    return y_new
 
 
 def sample(feature, new_n=100):
