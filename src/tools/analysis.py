c2c08197bbae07a47ee320fef1786af2879e798c
luanshiyinyang <1695735420@qq.com>
2020-06-26 17:31:01 +0800

    update analysis

--- src/tools/analysis.py
+++ src/tools/analysis.py
@@ -2,21 +2,20 @@
 三个数据集的数据分析
 视频长度 Segment的长度 每个Segment占视频的比例 每个视频的segment数目
 """
-
-
 import json
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-def statistic(gt, result):
+def statistic(gt, result, contrast=False):
+    # if contrast is True, show analysis about prediction result
 
     lengths = []
     segment_ratio = []
     segment_num = []
     act_labels = []
     test_duration = []
-    for k,v in gt.items():
+    for k, v in gt.items():
         if v['subset'] != 'testing':
             duration = v['duration']
             lengths.append(duration)
@@ -25,27 +24,27 @@ def statistic(gt, result):
             for act in v['annotations']:
                 start = act['segment'][0]
                 end = act['segment'][1]
-                segment_ratio.append((end-start)/duration)
+                segment_ratio.append((end - start) / duration)
                 act_labels.append(act['label'])
                 seg_num += 1
             segment_num.append(seg_num)
         else:
             test_duration.append(v['duration'])
 
-    return lengths,act_labels,segment_num,segment_ratio
+    return lengths, act_labels, segment_num, segment_ratio
 
 
 def load_info(filename):
-    with open(filename,'r') as f:
-        info  = json.load(f)
+    with open(filename, 'r') as f:
+        info = json.load(f)
     return info
 
 
-def draw(info,dataset):
+def draw(info, dataset):
     print('max:{}'.format(max(info)))  # 448 755
 
     bins = np.arange(0, 1.01, 0.05)
-    if dataset =='tianchi':
+    if dataset == 'tianchi':
         color = 'b'
     elif dataset == 'activitynet':
         color = 'fuchsia'
@@ -61,16 +60,16 @@ def draw(info,dataset):
 
 if __name__ == '__main__':
     dataset_name = 'activitynet'
-    if dataset_name =='tianchi':
+    if dataset_name == 'tianchi':
         gt_file = '../../data/Tianchi/train_annotations_new.json'
         result_file = '../../../results/DBG-0624-0956-tianchi-validation.json'
-    elif dataset_name=='activitynet':
+    elif dataset_name == 'activitynet':
         gt_file = '../../data/ActivityNet/video_info_19993.json'
         result_file = '../../../results/DBG-0623-1614-activitynet-validation.json'
-    elif dataset_name=='thumos':
+    elif dataset_name == 'thumos':
         gt_file = None
 
     gt = load_info(gt_file)
     result = load_info(result_file)
-    lengths, act_labels, segment_num, segment_ratio = statistic(gt,result)
+    lengths, act_labels, segment_num, segment_ratio = statistic(gt, result)
     draw(segment_ratio, dataset_name)
