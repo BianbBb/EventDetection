cf91ef236a63b27954326a868ab852a4c847cb1f
BianbBb <723631977@qq.com>
2020-07-03 15:18:41 +0800

    dataloader

--- src/models/detr/transformer.py
+++ src/models/detr/transformer.py
@@ -242,10 +242,18 @@ class TransformerDecoderLayer(nn.Module):
                               key_padding_mask=tgt_key_padding_mask)[0]
         tgt = tgt + self.dropout1(tgt2)
         tgt2 = self.norm2(tgt)
+
+        print('-------')
+        print(tgt2.size())
+        print('-------')
+
+
+
         tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt2, query_pos),
                                    key=self.with_pos_embed(memory, pos),
                                    value=memory, attn_mask=memory_mask,
-                                   key_padding_mask=memory_key_padding_mask)[0]
+                                   key_padding_mask=memory_key_padding_mask)[0] #### todo: tuple index out of range
+
         tgt = tgt + self.dropout2(tgt2)
         tgt2 = self.norm3(tgt)
         tgt2 = self.linear2(self.dropout(self.activation(self.linear1(tgt2))))
--- src/train.py
+++ src/train.py
@@ -12,8 +12,8 @@ if not torch.cuda.is_available():
 config = Config()
 
 # dataset
-train_dl = DataLoader(MyDataSet(config, mode='training'), batch_size=config.batch_size,
-                      shuffle=True, num_workers=0, drop_last=True, pin_memory=True)
+# train_dl = DataLoader(MyDataSet(config, mode='training'), batch_size=config.batch_size,
+#                       shuffle=True, num_workers=0, drop_last=True, pin_memory=True)
 # val_dl = DataLoader(MyDataSet(config, mode='validation'), batch_size=config.batch_size,
 #                     shuffle=False, num_workers=0, drop_last=True, pin_memory=True)
 
@@ -24,15 +24,15 @@ model = network(config)
 # # trainer.run()
 
 if __name__ == '__main__':
-    # from torchsummary import summary
-    # summary(model.cuda(),(100,1024),batch_size=2)
-    for n_iter, (gt_action, gt_start, gt_end, feature) in enumerate(train_dl):
-        torch.cuda.empty_cache()
-        print('-----')
-        print(gt_action)
-        print(gt_start)
-        print(gt_end)
-        #print(feature)
-        print(feature.size())
+    from torchsummary import summary
+    summary(model.cuda(),(100,1024),batch_size=2)
+    # for n_iter, (gt_action, gt_start, gt_end, feature) in enumerate(val_dl):
+    #     torch.cuda.empty_cache()
+    #     print('-----')
+    #     print(gt_action)
+    #     print(gt_start)
+    #     print(gt_end)
+    #     #print(feature)
+    #     print(feature.size())
 
 
