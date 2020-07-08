88b0d3b6abde8059e2a7cf5195191c70453e28e7
BianbBb <723631977@qq.com>
2020-07-08 21:15:58 +0800

    dataload

--- src/train.py
+++ src/train.py
@@ -15,38 +15,30 @@ config = Config()
 
 def collate_fn(batch):
     batch = list(zip(*batch))
-    #batch[0] = nested_tensor_from_tensor_list(batch[0])
     return tuple(batch)
-
-
 
 # dataset
 dataset_train = MyDataSet(config, mode='training')
-# dataset_val = MyDataSet(config, mode='validation')
+dataset_val = MyDataSet(config, mode='validation')
 
 # if args.distributed:
 #     sampler_train = DistributedSampler(dataset_train)
 #     sampler_val = DistributedSampler(dataset_val, shuffle=False)
 # else:
 sampler_train = torch.utils.data.RandomSampler(dataset_train)
-# sampler_val = torch.utils.data.SequentialSampler(dataset_val)
-#
+sampler_val = torch.utils.data.SequentialSampler(dataset_val)
+
 batch_sampler_train = torch.utils.data.BatchSampler(sampler_train, config.batch_size, drop_last=True)
-# batch_sampler_val = torch.utils.data.BatchSampler(sampler_train, config.batch_size, drop_last=False)
 
-#train_dl = DataLoader(MyDataSet(config, mode='training'), batch_size=config.batch_size,shuffle=True, num_workers=0, drop_last=True)
-# val_dl = DataLoader(MyDataSet(config, mode='validation'), batch_size=config.batch_size,shuffle=False, num_workers=0, drop_last=True)
-#
+train_dl = DataLoader(dataset_train, batch_sampler=batch_sampler_train,collate_fn=collate_fn, num_workers=0)
+val_dl = DataLoader(dataset_val, config.batch_size, sampler=sampler_val,drop_last=False, collate_fn=collate_fn, num_workers=0)
 
-train_dl = DataLoader(dataset_train, batch_sampler=batch_sampler_train,
-                               collate_fn=collate_fn, num_workers=0)
-#
+
+# model
+model = network(config)
 # #
-# # # model
-# model = network(config)
-# #
-# trainer = DetrTrainer(config, model, train_dl, val_dl)
-# trainer.run()
+trainer = DetrTrainer(config, model, train_dl, val_dl)
+trainer.run()
 
 
 if __name__ == '__main__':
@@ -59,4 +51,4 @@ if __name__ == '__main__':
         # for k,v in targets.items():
         #     print(k)
         #     print(v)
-
+        # 验证下collate_fn中的batch输出结果
